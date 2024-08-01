33ca7855a0502b79f271bcf32a19c1eaa037e913
Mohamed Haaris <160810363+Haarissams@users.noreply.github.com>
2024-08-01 18:29:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -18,17 +18,11 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-
-
-
-
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -58,19 +52,17 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    //User.insertMany(users);
-    //Post.insertMany(posts);
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
